2e54fa4bf978688060029359aacf6de14a9662cf
Md Maruf Hasan <marufhasanash@gmail.com>
2026-04-05 15:58:48 +0600

    Add project files.

---
+++ Blooging/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blooging.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
