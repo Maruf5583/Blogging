65e273d988089ee4f801c6735add325189d1add2
Md Maruf Hasan <marufhasanash@gmail.com>
2026-04-06 21:41:26 +0600

    add appdbcontext

--- Blooging/Data/AppDbContext.cs
+++ Blooging/Data/AppDbContext.cs
@@ -15,6 +15,64 @@ namespace Blooging.Data
 
         public DbSet<Comment> Comments { get; set; }
 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
         
+                base.OnModelCreating (modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData (
+                
+                new Category { Id = 1, Name = "Technology", Description = "Posts related to technology and gadgets." },
+                 
+                new Category { Id = 2, Name = "Travel", Description = "Posts about travel experiences and tips." },
+
+                new Category { Id = 3, Name = "Food", Description = "Posts about recipes, restaurants, and food culture." }
+                );
+
+
+            modelBuilder.Entity<Post>().HasData(
+
+    new Post
+    {
+        Id = 1,
+        Title = "The Future of AI",
+        Content = "Artificial Intelligence is rapidly evolving...",
+        Author = "Jhon Smith",
+        FeaturesImagePath = "img1.png",
+        PublishedDate = new DateTime(2026, 1, 1),
+        CategoryId = 1
+    },
+
+    new Post
+    {
+        Id = 2,
+        Title = "Top 10 Travel Destinations for 2024",
+        Content = "Looking for your next travel adventure...",
+        Author = "Jhon Smith",
+        FeaturesImagePath = "img2.png",
+        PublishedDate = new DateTime(2026, 1, 2),
+        CategoryId = 2
+    },
+
+    new Post
+    {
+        Id = 3,
+        Title = "Delicious Vegan Recipes",
+        Content = "Discover delicious vegan recipes...",
+        Author = "Jhon Smith",
+        FeaturesImagePath = "img3.png",
+        PublishedDate = new DateTime(2026, 1, 3),
+        CategoryId = 3
+    }
+);
+
+
+
+
+
+
+
+        }
     }
 }
---
+++ Blooging/Migrations/20260406055141_seeddatas.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Blooging.Migrations
+{
+    /// <inheritdoc />
+    public partial class seeddatas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Posts related to technology and gadgets.", "Technology" },
+                    { 2, "Posts about travel experiences and tips.", "Travel" },
+                    { 3, "Posts about recipes, restaurants, and food culture.", "Food" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Author", "CategoryId", "Content", "FeaturesImagePath", "PublishedDate", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Jhon Smith", 1, "Artificial Intelligence is rapidly evolving...", "img1.png", new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "The Future of AI" },
+                    { 2, "Jhon Smith", 2, "Looking for your next travel adventure...", "img2.png", new DateTime(2026, 1, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "Top 10 Travel Destinations for 2024" },
+                    { 3, "Jhon Smith", 3, "Discover delicious vegan recipes...", "img3.png", new DateTime(2026, 1, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "Delicious Vegan Recipes" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
